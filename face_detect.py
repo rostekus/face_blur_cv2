a1ee6bfcd9304d677e3b82244a8740edb5addd55
Rostyslav Mosorov <rostyslavmosorov@gmail.com>
2021-05-29 12:00:28 +0200

    added pixel blur

--- face_detect.py
+++ face_detect.py
@@ -1,18 +1,51 @@
 import cv2
 
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+DRAW_RECTANGLE = True
 
+
+
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 webcam = cv2.VideoCapture(0)
 
 while True:
 
 
     is_successful_frame , frame = webcam.read()
+    result_image = frame.copy()
 
     if is_successful_frame:
-        gray_scaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('hello', gray_scaled_img)
-        cv2.waitKey(1)
+        grayimg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        faces = trained_face_data.detectMultiScale(grayimg, scaleFactor = 1.1, minNeighbors = 6)
+
+        if len(faces) != 0:         # If there are faces in the images
+            for f in faces:         # For each face in the image
+
+                # Get the origin co-ordinates and the length and width till where the face extends
+                x, y, w, h = [ v for v in f ]
+
+                # get the rectangle img around all the faces
+               
+                
+                
+                sub_face = frame[y:y+h, x:x+w]
+                height, width = sub_face.shape[:2]
+                w, h = (16, 16)
+                temp = cv2.resize(sub_face, (w, h), interpolation=cv2.INTER_LINEAR)
+                sub_face = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+                # apply a gaussian blur on this new recangle image
+                # merge this blurry rectangle to our final image
+                result_image[y:y+sub_face.shape[0], x:x+sub_face.shape[1]] = sub_face
+        cv2.imshow('hello',result_image)
+    key = cv2.waitKey(1)
+
+        
+    if key == ord('q'):
+        break
+webcam.release()
+
+
+
+      
 
 
 
